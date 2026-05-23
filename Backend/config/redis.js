55d32917e6016eb3928aaa6be33589fbfa4abef8
AnkitaGodara <ankitagodara79@gmail.com>
2026-05-23 10:54:51 +0530

    removed redis

--- Backend/config/redis.js
+++ Backend/config/redis.js
@@ -1,18 +1,1 @@
-const IORedis = require("ioredis");
-
-const connection = new IORedis({
-  host: "127.0.0.1",
-  port: 6379,
-
-  maxRetriesPerRequest: null,
-});
-
-connection.on("connect", () => {
-  console.log("Redis Connected");
-});
-
-connection.on("error", (err) => {
-  console.log("Redis Error:", err);
-});
-
-module.exports = connection;
+module.exports = {};
--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "ai-task-processing-platform-production.up.railway.app/api",
+  baseURL: "https://ai-task-processing-platform-production.up.railway.app/api",
 });
 
 export default API;
